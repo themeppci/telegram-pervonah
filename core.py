63b3ce51bf9e696ddf2452a5416c88c5e44eed70
themeppci <128810079+themeppci@users.noreply.github.com>
2023-08-15 14:25:58 +0300

    Update core.py

--- core.py
+++ core.py
@@ -2,7 +2,9 @@ from pyrogram import Client
 from pyrogram import types, filters
 import time
 
+delay = 1
 comment = 'я в ахуи'
+
 api_id = 123
 api_hash = "123abc"
 
@@ -17,7 +19,7 @@ app = Client(
 @app.on_message(filters=filters.forwarded)
 def my_handler(client, message):
     print(f'NEW POST :: {message.sender_chat.title}')
-    time.sleep(11)
+    time.sleep(delay)
     app.send_message(
         chat_id=message.chat.id,
         text=comment,
